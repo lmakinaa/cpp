cb2ab3ecb7f3227a5125611a5d7d6a485d6e792a
lmakinaa <ijaija@e1r2p15.1337.ma>
2024-06-04 20:36:19 +0100

    hmm

---
+++ module-00/ex01/PhoneBook.cpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 11:31:14 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/04 20:34:54 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook()
+{}
+
+void PhoneBook::runPhoneBook()
+{
+	std::string line;
+	index = 0;
+	while (1)
+	{
+		std::cout << "$> " << std::flush;
+		std::getline(std::cin, line);
+		if (line.compare("ADD") == 0)
+			addCommand();
+		//else if (line.compare("SEARCH") == 0)
+		//	searchCommand();
+		//else if (line.compare("EXIT") == 0)
+		//	exitCommand();
+	}
+}
+
+void PhoneBook::addCommand()
+{
+	// what should the user enter.
+	int phase = 0;
+
+	while (phase != 5)
+	{
+		switch(phase)
+		{
+			case 0:
+				std::cout << "Enter first name: " << std::flush;
+				std::cin >> m_firstName;
+				phase = 1;
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max());
+				break ;
+			case 1:
+				std::cout << "Enter last name: " << std::flush;
+				std::cin >> m_lastName;
+				phase = 2;
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max());
+				break ;
+			case 2:
+				std::cout << "Enter nickname: " << std::flush;
+				std::cin >> m_nickname;
+				phase = 3;
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max());
+				break ;
+			case 3:
+				std::cout << "Enter phone number: " << std::flush;
+				std::getline(std::cin, m_phoneNum);
+				int	i = 0;
+				while (m_phoneNum[i] && !isdigit(m_phoneNum[i]))
+					i++;
+				if (m_phoneNum[i] == '\0')
+					phase = 4;
+				break ;
+			case 4:
+				std::cout << "Enter darkest secret: " << std::flush;
+				std::getline(std::cin, m_darkestSecret);
+				phase = 5;
+				break ;
+		}
+	}
+	contacts[index] = Contact(m_firstName, m_nickname,  m_lastName, m_phoneNum, m_darkestSecret);
+	index++;
+	if (index == 8)
+		index = 0;
+	contacts[index].print();
+}
---
+++ module-00/ex01/PhoneBook.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 17:40:30 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/04 20:35:13 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_HPP
+#define PHONE_BOOK_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+#include "Contact.hpp"
+
+class PhoneBook
+{
+private:
+	Contact contacts[8];
+
+	// the offset that indicate the current position in the contacts array
+	int index;
+
+	std::string m_firstName;
+	std::string m_lastName;
+	std::string m_nickname;
+	std::string m_phoneNum;
+	std::string m_darkestSecret;
+
+public:
+	PhoneBook();
+	void addCommand();
+	//void searchCommand();
+	//void exitCommand();
+	void runPhoneBook();
+};
+
+#endif
