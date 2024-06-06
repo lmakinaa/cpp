e6c5fef59899f5608092b386138cdd91e9347c24
lmakinaa <ijaija@e2r8p5.1337.ma>
2024-06-06 17:32:22 +0100

    hmm

--- module-01/ex00/main.cpp
+++ module-01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 16:10:46 by ijaija            #+#    #+#             */
-/*   Updated: 2024/06/06 16:14:17 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/06/06 17:32:15 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int main()
 	Zombie* z1 = newZombie("Zombie-N1");
 	z1->announce();
 	randomChump("Zombie-N2");
-	//z1->~Zombie();
 	delete z1;
+	return 0;
 }
