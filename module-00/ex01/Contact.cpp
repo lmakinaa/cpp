7e6ffc5b56a0b2f872e5629b93ed88b8dcaa8665
lmakinaa <ijaija@e1r2p15.1337.ma>
2024-06-07 00:00:00 +0100

    hmm

---
+++ module-00/ex01/Contact.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 17:37:51 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/04 20:32:13 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+Contact::Contact(std::string firstName,
+				 std::string lastName,
+				 std::string nickname,
+				 std::string phoneNum,
+				 std::string darkestSecret)
+{
+	m_firstName = firstName;
+	m_lastName = lastName;
+	m_nickname = nickname;
+	m_phoneNum = phoneNum;
+	m_darkestSecret = darkestSecret;
+}
+
+void Contact::print()
+{
+	std::cout << "First Name: " << m_firstName << std::endl;
+	std::cout << "Last Name: " << m_lastName << std::endl;
+	std::cout << "Nickname: " << m_nickname << std::endl;
+	std::cout << "Phone Number: " << m_phoneNum << std::endl;
+	std::cout << "Darkest Secret: " << m_darkestSecret << std::endl;
+}
+	
---
+++ module-00/ex01/Contact.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 17:39:26 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/04 20:31:33 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <iostream>
+#include <iomanip>
+
+class Contact
+{
+private:
+	std::string m_firstName;
+	std::string m_lastName;
+	std::string m_nickname;
+	std::string m_phoneNum;
+	std::string m_darkestSecret;
+
+public:
+	Contact(std::string firstName,
+			std::string lastName,
+			std::string nickName,
+			std::string phoneNum,
+			std::string darkestSecret);
+	void print();
+};
+
+
+#endif
