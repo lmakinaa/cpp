3f689c4df39eceba9e395b9dbff26b92ef160b3c
lmakinaa <ijaija@e2r8p5.1337.ma>
2024-06-06 18:21:27 +0100

    hmm

---
+++ module-01/ex02/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 18:08:48 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/06 18:21:18 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+/*
+* The only difference between a pointer and referance is the syntax.
+* The ref do the job of pointers implicitly.
+* Also ref should be defined on declaration and cannot be changed after that
+* to reference another var.
+*/
+int	main()
+{
+	// definitions
+	std::string string = "HI THIS IS BRAIN";
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+	// addresses
+	std::cout << &string << '\n';
+	std::cout << stringPTR << '\n';
+	std::cout << &stringREF << '\n';
+	// values
+	std::cout << string << '\n';
+	std::cout << *stringPTR << '\n';
+	std::cout << stringREF << '\n';
+	return 0;
+}
