97bfb542b0b428bf018819a694925feef953cf3b
lmakinaa <ijaija@e2r10p3.1337.ma>
2024-06-08 06:20:16 +0100

    hmm

--- module-01/ex06/main.cpp
+++ module-01/ex06/main.cpp
@@ -5,28 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/07 17:56:30 by ijaija            #+#    #+#             */
-/*   Updated: 2024/06/08 06:19:08 by ijaija           ###   ########.fr       */
+/*   Created: 2024/06/07 21:19:01 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/07 21:19:02 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "File.hpp"
-
-void f()
-{
-	system("lsof -c freplace");
-}
-//read all file
-int main(int ac, char **av)
-{
-	atexit(f);
-	if (ac != 4)
-	{
-		std::cout << "Usage: ./freplace <filename> <match string> <substitute>\n";
-		return 1;
-	}
-	File f(av[1]);
-	if (f.createReplaceFile(av[2], av[3]) == 1)
-		return (1);
-	return 0;
-}
