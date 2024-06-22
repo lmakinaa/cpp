ff21fbd866c2dfad9f59a2d80e994e97fb3dfe29
Lmakinaa <jaija.ismail@gmail.com>
2024-06-22 19:25:26 +0100

    hmm

---
+++ module-03/ex01/ClapTrap.cpp
@@ -0,0 +1,92 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(const char* name) 
+    : m_name (name)
+    , m_hitPts (10)
+    , m_energyPts (10)
+    , m_attackDamage (0)
+{
+    std::cout << name << " has been created\n";
+}
+
+ClapTrap::ClapTrap(const ClapTrap& src)
+    : m_name (src.m_name)
+    , m_hitPts (src.m_hitPts)
+    , m_energyPts (src.m_energyPts)
+    , m_attackDamage (src.m_attackDamage)
+{
+    std::cout << m_name << " has been created\n";
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& src)
+{
+    if (this == &src)
+        return *this;
+
+    m_name = src.m_name;
+    m_energyPts = src.m_energyPts;
+    m_hitPts = src.m_hitPts;
+    m_attackDamage = src.m_attackDamage;
+
+    return *this;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << m_name << " has been destroyed\n";
+}
+
+// getters
+
+const std::string& ClapTrap::getName() const
+{
+    return (m_name);
+}
+
+unsigned int ClapTrap::getAttackDamage() const
+{
+    return (m_attackDamage);
+}
+
+// Other function members
+
+void ClapTrap::attack(const std::string& target)
+{
+    if (m_energyPts < 1)
+    {
+        std::cout << m_name << " has no energy points\n";
+        return ;
+    }
+    m_energyPts -= 1;
+    std::cout << "ClapTrap " << m_name << " attacked "
+        << target << ", causing " << m_attackDamage
+        << " points of damage!\n";
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if (m_hitPts < amount)
+    {
+        std::cout << "ClapTrap " << m_name
+            << " hasn't enought hit points\n";
+        return ;
+    }
+    m_hitPts -= amount; 
+    std::cout << "ClapTrap " << m_name << " took " << amount
+        << " of damage!\n";
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if (m_energyPts < amount)
+    {
+        std::cout << "ClapTrap " << m_name
+            << " hasn't enought energy points\n";
+        return ;
+    }
+    m_energyPts -= amount;
+    m_hitPts += amount;
+    std::cout << "ClapTrap " << m_name
+        << " has gained " << amount << " hit points that cost him "
+        << amount << " of energy points!\n";
+}
---
+++ module-03/ex01/ScavTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+#include <string>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+
+public:
+    ScavTrap(const char* name);
+    ~ScavTrap();
+    void attack(const std::string& target);
+    void guardGate() const;
+
+};
+
+#endif
