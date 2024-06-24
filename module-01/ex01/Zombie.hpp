7801369d4120e33fe318ee18db06a12e37ffa893
lmakinaa <ijaija@e2r8p5.1337.ma>
2024-06-25 00:00:00 +0100

    hmm

---
+++ module-01/ex01/Zombie.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 15:25:37 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/06 17:49:19 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+	
+private:
+	std::string m_name;
+
+public:
+	Zombie();
+	~Zombie();
+	void announce();
+	void setName(std::string name);
+	
+};
+
+Zombie* zombieHorde(int N, std::string name);
+
+#endif
