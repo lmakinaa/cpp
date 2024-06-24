b5527525968d817c362acceb7fed6551066b4291
Lmakinaa <jaija.ismail@gmail.com>
2024-06-24 21:30:25 +0100

    hmm

---
+++ module-04/ex00/Cat.hpp
@@ -0,0 +1,18 @@
+#ifndef CAT_HPP
+# define CAT_HPP
+
+#include <iostream>
+#include <string>
+#include "Animal.hpp"
+
+class Cat : public Animal
+{
+
+public:
+    Cat();
+    ~Cat();
+    void makeSound() const;
+
+};
+
+#endif
---
+++ module-04/ex00/main.cpp
@@ -0,0 +1,50 @@
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
+#include "WrongAnimal.hpp"
+#include <cstdlib>
+
+
+void f()
+{
+    system("leaks polymorphism");
+}
+
+int main()
+{
+    atexit(f);
+
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); //will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+
+    delete meta;
+    delete j;
+    delete i;
+
+    // Testing Wrong animal and Cat
+    std::cout << "\n\t\t Wrong Animal \t\t\n" << std::endl;
+    
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongI = new WrongCat();
+    const WrongCat* wrongCat = new WrongCat();
+
+    std::cout << wrongI->getType() << std::endl;
+    
+    wrongI->makeSound();
+    wrongMeta->makeSound();
+    wrongCat->makeSound();
+
+
+    delete wrongMeta;
+    delete wrongI;
+    delete wrongCat;
+
+    return 0;
+}
