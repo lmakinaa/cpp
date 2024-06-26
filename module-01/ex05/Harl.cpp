0471213401ba7b3f228f674b452e6510531a7feb
lmakinaa <ijaija@e2r10p3.1337.ma>
2024-06-27 00:00:00 +0100

    hmm

---
+++ module-01/ex05/Harl.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 16:49:29 by ijaija            #+#    #+#             */
+/*   Updated: 2024/06/07 17:54:21 by ijaija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+}
+
+Harl::~Harl()
+{
+}
+
+void Harl::debug()
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void Harl::info()
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning()
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.\n";
+}
+
+void Harl::error()
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain(std::string level)
+{
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	Harl::t_m_func actions[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	
+	for (int i = 0; i < 4; i++)
+		if (levels[i].compare(level) == 0)
+			(this->*actions[i])();
+}
