91a80f59f641cef0799949b7c6dc240a94183223
lmakinaa <ijaija@e2r9p5.1337.ma>
2024-06-29 00:00:00 +0100

    hmm

--- module-01/ex06/Harl.cpp
+++ module-01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 16:49:29 by ijaija            #+#    #+#             */
-/*   Updated: 2024/06/07 21:49:01 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/06/09 21:19:42 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,29 @@ Harl::~Harl()
 
 void Harl::debug()
 {
+	std::cout << "[ DEBUG ]\n";
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
 }
 
 void Harl::info()
 {
+	std::cout << "[ INFO ]\n";
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 
 void Harl::warning()
 {
 	std::cout << "[ WARNING ]\n";
-	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month.\n\n";
+	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month.\n";
 }
 
 void Harl::error()
 {
+	std::cout << "[ ERROR ]\n";
 	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
 }
 
-void Harl::startComplaining(std::string& level)
+void Harl::startComplaining(std::string level)
 {
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	int c = -1;
@@ -53,11 +56,10 @@ void Harl::startComplaining(std::string& level)
 			break ;
 		}
 	}
-	
+
 	switch (c)
 	{
 	case 0:
-		std::cout << "[ WARNING ]";
 		serieOfComplain(0);
 		break;
 	case 1:
@@ -70,8 +72,20 @@ void Harl::startComplaining(std::string& level)
 		serieOfComplain(3);
 		break;
 	default:
-		
+		std::cout << "[ Probably complaining about insignificant problems ]\n";
 		break;
+	}
+}
+
+void Harl::serieOfComplain(int c)
+{
+	t_m_func actions[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	while (c < 4)
+	{
+		(this->*actions[c])();
+		std::cout << '\n';
+		c++;
 	}
 }
 
