aefc6edeb84088a47ac2843b0776ed69f8c0466c
Ismail <jaija.ismail@gmail.com>
2024-06-30 00:00:00 +0100

    hmm

--- module-02/ex01/Fixed.hpp
+++ module-02/ex01/Fixed.hpp
@@ -8,17 +8,29 @@ class Fixed
 {
 
 public:
+    // constructors
     Fixed();
     Fixed(const Fixed& src);
+    Fixed(const int intNum);
+    Fixed(const float floatNum);
+
+    // destructors
     ~Fixed();
+
+    // operators
     Fixed& operator=(const Fixed& src);
+
     int getRawBits() const;
     void setRawBits(int const raw);
+    float toFloat() const;
+    int toInt() const;
 
 private:
     static const int m_frac_bits = 8;
     int m_value;
 
 };
+
+std::ostream& operator<<(std::ostream& out, const Fixed& inFixedPoint);
 
 #endif
