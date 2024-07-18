8b4120153421bb2b2d2e64fba0b643b6efcbf4da
Lmakinaa <jaija.ismail@gmail.com>
2024-07-19 00:00:00 +0100

    added more test and canonical

--- module-04/ex00/Dog.cpp
+++ module-04/ex00/Dog.cpp
@@ -11,8 +11,23 @@ Dog::~Dog()
     std::cout << "Dog instance has been destructed.\n";
 }
 
+Dog::Dog(const Dog& src)
+    : Animal()
+{
+    *this = src;
+    std::cout << "Dog instance has been copy constructed.\n";
+}
+
+Dog& Dog::operator=(const Dog& src)
+{
+    if (this == &src)
+        return *this;
+    type = src.type;
+    return *this;
+}
+
 void Dog::makeSound() const
 {
-    std::cout << "WOOF 7AYD MN HNA\n";    
+    std::cout << "WOOF WOOF\n";    
 }
 
