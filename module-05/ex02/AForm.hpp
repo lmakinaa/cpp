e4bef2e872bbe2802dbd0951913c47f6f619045f
Lmakinaa <jaija.ismail@gmail.com>
2024-07-31 00:00:00 +0100

    finished ex02

---
+++ module-05/ex02/AForm.hpp
@@ -0,0 +1,68 @@
+#ifndef AFORM_HPP
+# define AFORM_HPP
+
+#ifndef NOEXCEPT
+# define NOEXCEPT throw()
+#endif
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class AForm
+{
+
+public:
+	class GradeTooHighException: public std::exception
+	{
+	public:
+		GradeTooHighException(std::string error);
+		~GradeTooHighException() NOEXCEPT;
+		const char* what() const NOEXCEPT;
+	private:
+		std::string m_error;
+	};
+
+	class GradeTooLowException: public std::exception
+	{
+	public:
+		GradeTooLowException(std::string error);
+		~GradeTooLowException() NOEXCEPT;
+		const char* what() const NOEXCEPT;
+	private:
+		std::string m_error;
+	};
+
+public:
+	AForm(std::string name, int gradeToExec, int gradeToSign);
+
+	AForm();
+	AForm(const AForm& src);
+	AForm& operator=(const AForm& src);
+	~AForm();
+
+	const std::string& getName() const;
+	int getGradeToExecute() const;
+	int getGradeToSign() const;
+	bool getSigned() const;
+	void beSigned(Bureaucrat& worker);
+	void execute(Bureaucrat const & executor) const;
+	virtual void execForm() const = 0;
+
+protected:
+	std::string m_task;
+
+private:
+	const std::string m_name;
+	const int m_gradeToExecute;
+	const int m_gradeToSign;
+	bool m_signed;
+
+};
+
+std::ostream& operator<<(std::ostream& out, const AForm& in);
+
+#endif
