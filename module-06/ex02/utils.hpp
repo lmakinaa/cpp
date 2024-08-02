955a08fef09187f65586aa806d9a2162dcf0a83c
Lmakinaa <jaija.ismail@gmail.com>
2024-08-03 00:00:00 +0100

    finished module06

---
+++ module-06/ex02/utils.hpp
@@ -0,0 +1,15 @@
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base*	generate(void);
+void	identify(Base* p);
+void	identify(Base& p);
+
+#endif
