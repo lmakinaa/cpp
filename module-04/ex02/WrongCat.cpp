45f301d36b460759d2baa6c8dfbcce5ff4911ec4
Ismail Jaija <ijaija@e3r1p6.1337.ma>
2024-08-05 15:07:04 +0100

    updated classes

--- module-04/ex02/WrongCat.cpp
+++ module-04/ex02/WrongCat.cpp
@@ -12,6 +12,21 @@ WrongCat::~WrongCat()
     std::cout << "WrongCat instance has been destructed.\n";
 }
 
+WrongCat::WrongCat(const WrongCat& src)
+    : WrongAnimal()
+{
+    *this = src;
+    std::cout << "WrongCat instance has been copy constructed.\n";
+}
+
+WrongCat& WrongCat::operator=(const WrongCat& src)
+{
+    if (this == &src)
+        return *this;
+    type = src.type;
+    return *this;
+}
+
 void WrongCat::makeSound() const
 {
     std::cout << "WRONG MEOW MEOW NI**A\n";
