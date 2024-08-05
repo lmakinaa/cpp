c88ede88772e3bc7ec8fe72f019ec3fc10cfadaf
Ismail Jaija <ijaija@e3r1p6.1337.ma>
2024-08-05 15:04:54 +0100

    hmm

--- module-04/ex01/Dog.cpp
+++ module-04/ex01/Dog.cpp
@@ -35,16 +35,8 @@ Dog& Dog::operator=(const Dog& src)
 {
     if (this == &src)
         return *this;
-    delete m_brain;
-    try 
-    {
-        m_brain = new Brain(*src.m_brain);
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Exception caught: " << e.what() << '\n'; 
-        std::exit(1);
-    }
+    
+    *m_brain = *(src.m_brain);
     type = src.type;
     std::cout << "Dog copy assignement called.\n";
     return *this;
@@ -61,3 +53,12 @@ void Dog::makeSound() const
     std::cout << "WOOF WOOF\n";    
 }
 
+void Dog::setIdea(std::string idea, int index)
+{
+    m_brain->setIdea(idea, index);
+}
+
+void Dog::showIdeas()
+{
+    m_brain->showIdeas();
+}
