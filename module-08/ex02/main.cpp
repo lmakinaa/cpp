d3111400b13d8e4cf1601bd8de75441bf2e8ada3
Lmakinaa <jaija.ismail@gmail.com>
2024-10-02 21:19:05 +0100

    finished ex02

--- module-08/ex02/main.cpp
+++ module-08/ex02/main.cpp
@@ -4,26 +4,41 @@
 
 int main()
 {
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
-//[...]
-mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin();
-MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+    {
+        MutantStack<int> x;
+
+        x.push(7);
+        x.push(76);
+        x.push(3);
+        x.push(310);
+        x.push(13);
+        x.push(88);
+
+        MutantStack<int> y = x;
+
+        for (MutantStack<int>::iterator i = y.begin(); i != y.end(); i++) {
+            std::cout << *i << '\n';
+        }
+    }
+
+    std::cout << "\nReplacing MutantStack by std::list\n" << std::endl;
+    
+    {
+        std::list<int> x;
+
+        x.push_back(7);
+        x.push_back(76);
+        x.push_back(3);
+        x.push_back(310);
+        x.push_back(13);
+        x.push_back(88);
+
+        std::list<int> y = x;
+
+        for (std::list<int>::iterator i = y.begin(); i != y.end(); i++) {
+            std::cout << *i << '\n';
+        }
+    }
+
+    return 0;
 }
